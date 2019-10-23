64f71a52104d3db80b20a3e0c0f1e56f154aa497
amirlamri <amir.collab@gmail.com>
2019-10-23 17:39:40 +0100

    commit branch 2

--- test/src/test/TestMainMaster.java
+++ test/src/test/TestMainMaster.java
@@ -1,11 +1,10 @@
 package test;
 
-public class TestMain {
+public class TestMainMaster {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.print("hey");
-      System.out.print("test3");
+
 	}
 
 }
